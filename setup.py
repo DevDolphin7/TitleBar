d8d319e159837790f9b871a4edbaedf525784226
DevDolphin7 <DevDolphin7@users.noreply.github.com>
2024-02-22 22:22:40 +0000

    Added Descriptions
    Added descriptions to setup.py to show on PyPI

--- setup.py
+++ setup.py
@@ -9,4 +9,11 @@ setup(
         'pillow',
         'customtkinter',
     ],
+    description="Create a custom title bar for a windows app developed with custom tkinter",
+    long_description="""pip install TitleBarCTk
+
+Create a custom title bar for a windows app developed with custom tkinter.
+
+For futher information go to: https://github.com/DevDolphin7/TitleBar
+Or get in contact at DevDolphin7@outlook.com"""
 )
